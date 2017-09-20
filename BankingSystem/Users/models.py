3675fe5d5257b653802312f8429151423f86f036
Brandon Anderson <Brandon753.ba@gmail.com>
2017-09-20 15:08:43 -0700

    Initial Model layout for database design

--- BankingSystem/Users/models.py
+++ BankingSystem/Users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class Employee(models.model):
+    None
+
+
+class Individual(Profile):
+    None
+
+
+class Merchant(Profile):
+    None

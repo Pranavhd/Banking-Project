f559b5d0b7879e014eb658c80ceeb782b5aacc96
Brandon Anderson <Brandon753.ba@gmail.com>
2017-10-03 18:02:25 -0700

    Added Request Models
    Added request models for handling money transfers.
    
    Signed-Off-By: Brandon Anderson

--- BankingSystem/Request/models.py
+++ BankingSystem/Request/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
+from BankingSystem.Users.models import Profile
+
 
 # Create your models here.
+class Request(models.Model):
+    requester = models.ForeignKey('Users.Profile', on_delete=models.CASCADE, related_name="requester")
+
+
+class MoneyRequest(Request):
+    reciever = models.ForeignKey('Users.Profile', on_delete=None, related_name="reciever")
+    transfer_ammount = MoneyField(max_digits=20, decimal_places=2, default_currency='USD')
+    direction = (
+        ("send", 'Send'),
+        ("recieve", 'Recieve'),
+    )
--- BankingSystem/settings.py
+++ BankingSystem/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'BankingSystem.Users',
     'BankingSystem.Logs',
+    'BankingSystem.Request',
 ]
 
 MIDDLEWARE = [

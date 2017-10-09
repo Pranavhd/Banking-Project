f762f26f3f38567a7085985a76718c2a7f775d0d
Ravi Upmaka <rupmaka@asu.edu>
2017-10-09 01:44:55 -0700

    Updating Credit card model in accounts project

--- BankingSystem/Accounts/models.py
+++ BankingSystem/Accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.models.fields import MoneyField
 from django.core.exceptions import ValidationError
-from Users.models import Profile
+from BankingSystem.Users.models import Profile
 
 
 class BankingAccount(models.Model):
@@ -24,3 +24,6 @@ class CreditCard(BankingAccount):
             raise ValidationError("{0} is not the correct length".format(value), code=None, params=None)
     credit_card_number = models.CharField(max_length=19, unique=True, validators=[validate_length])
     ccv_number = models.CharField(max_length=5)
+    credit_limit = models.IntegerField()
+    bill_due_date = models.DateTimeField()
+    bill_due_amount = MoneyField(max_digits=20, decimal_places=2, default_currency='USD')
--- BankingSystem/Request/views.py
+++ BankingSystem/Request/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from BankingSystem.Accounts import models as accountInfo
 
 # Create your views here.
+# defining view for getting personal details information
+def credit_limit_increase_view(request):
+    user = User.objects.first()  # TODO: get the currently logged in user
+    credit_info = accountInfo.objects.get(user=user)
+
+

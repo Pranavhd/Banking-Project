fb1392f05c3dad560e3ecd3d795f8a403bd646de
Steve <steve.liushihao@gmail.com>
2017-10-29 13:39:12 -0700

    fix errors

--- BankingSystem/Users/views.py
+++ BankingSystem/Users/views.py
@@ -9,7 +9,7 @@ import collections
 from django.db.models import Q
 import random
 import string
-
+import decimal
 
 
 RenderUser = collections.namedtuple(
@@ -41,7 +41,7 @@ def count_user_penalty(user):
 
         if user.credit_balance < 0:
             before_credit_balance = user.credit_balance
-            after_credit_balance = user.credit_balance * 1.02 - 20
+            after_credit_balance = user.credit_balance * decimal.Decimal(1.02) - decimal.Decimal(20)
 
             models.Request.objects.create(
                 from_id=user.id,
